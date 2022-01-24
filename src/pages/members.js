529aa7210eb4a33b239aeebecb1826903a6d8007
sa-y-an <sayan.ee.nitd@gmail.com>
2022-01-24 22:28:55 +0530

    - Initial Draft - 4 paths - TBD - Fetch Members by Rest API/Json - Fetch Notices by RestAPI/Json - Make Templates for Evenets - Authored by - <sayan.ee.nitd@gmail.com>

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout2.js'
+import Team from '../sections/our-team'
 
 export default function members() {
   return (
     <Layout>
-
+      
+      <Team/>
+    
     </Layout>
   );
 }
--- src/pages/reports.js
+++ src/pages/reports.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout2.js'
-import { Container} from 'theme-ui';
+import { Container, Box} from 'theme-ui';
 
 export default function reports() {
   return (
@@ -10,8 +10,31 @@ export default function reports() {
           <Container>
             <div className="container" style={{minHeight : "60vh",width : "100vw"}}>
               <h2>
-                Notices
+                Year Wise Annual Reports
               </h2>
+              <Box p={2}>
+
+                <table style={{  border: "1px solid black", padding:"2px"}}>
+                  <tbody style={{padding:"5px"}} >
+                    <tr style={{padding:"5px"}}>
+                      <th style={{  border: "1px solid black",}}>
+                        Year
+                      </th>
+                      <th style={{  border: "1px solid black",}}>
+                        Link
+                      </th>
+                    </tr>
+                    <tr style={{padding:"5px"}}>
+                      <td style={{  border: "1px solid black",padding:"10px"}}>
+                        2019
+                      </td>
+                      <td style={{  border: "1px solid black", padding:"10px"}}>
+                        <a href="https://ewh.ieee.org/r10/calcutta/deis/doc/Annual_Report_DEIS_2018.pdf">Annual Report 2018</a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                </Box>
             </div>
           </Container>
           <hr />
