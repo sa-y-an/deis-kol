a183276a13e67a803cefedd950ecfe23a03f2536
sa-y-an <sayan.ee.nitd@gmail.com>
2022-01-24 06:35:05 +0530

    members

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Layout from '../components/layout.js'
+import { Container,Box } from 'theme-ui';
 
 export default function members() {
   return (
-      <div>
-          Members 
-      </div>
+      
+        <Layout>
+          <Container>
+            <br /><br /><br /><br /><br /><br /><br />
+            <div className="container" style={{minHeight : "60vh",width : "100vw"}}>
+              <h2>
+                Members 
+              </h2>
+            </div>
+          </Container>
+        </Layout>      
+      
   );
 }
+
